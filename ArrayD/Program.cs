9f39f4323b2092ea17c5b1d1f0e3bcd3856a1d29
Moussa <salisoumoussa1@gmail.com>
2025-11-11 11:01:44 +0100

    Creazione del progetta Array 3D

---
+++ ArrayD/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ArrayCollection;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        //3D: Creare, Leggere, Modificare, iterare foreach e for
+        string[,,] codici = new string[3, 2, 2]
+        {
+            {
+                { "100", "101"}, // Riga 0 colonna 0
+                { "102", "103"}, // Riga 1 colonna 1
+            },
+            {
+                { "201", "202"},
+                { "203", "204" }
+            },
+            {
+                { "405", "262"},
+                { "503", "904" }
+            }
+        };
+        // git clone https://github.com/salisou/SolutionALD_File.git
+
+        // Leggiamo
+
+        // Modificare 
+
+
+        // Iterazione con forheach e for 
+    }
+}
