32912cca5a2cbefcaa58ded93f03cca429b8b2a7
Moussa <salisoumoussa1@gmail.com>
2025-11-12 09:15:56 +0100

    Create new progeect

---
+++ EsportareDatiExcel/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+// (ClosedXML, DocumentFormat.OpenXml) Librerie da istallare per creare file Excel facilmente
+
+/*
+   Titolo: Esportare i dati in un file Excel con i metodi
+
+   Obiettivo:
+        Creare un file ReportUniversita.xlsx con più fogli:
+        Studenti
+        Corsi
+        Iscrizioni
+        Voti
+        Media per studente
+ */
+
+
+namespace EsportareDatiExcel;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
