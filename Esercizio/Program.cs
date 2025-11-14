2a9c8e71509afddae7db23767b24af46be00ff2f
Moussa <salisoumoussa1@gmail.com>
2025-11-14 10:51:50 +0100

    Created Project for Excercises

---
+++ Esercizio/Program.cs
@@ -0,0 +1,11 @@
+﻿/*
+ * Crea prima la truttura di base del program
+ *    Aggiungi un messagio di (Venvenuto nel ristorate)
+ *    Chiedi all'utente se ha una riservazione (si/on)
+ *    Se l'utente ristponde si (Con che nome?)
+ *          l'utente scrive il suo mome 
+ *          Messaggio (Ok (Nome dell'utente).......)          
+ *     , no(Non habbiamo tavoli liberi per ora)
+ *    Dopo la risposta dell'utente:
+ *      Presenta il menu
+*/
