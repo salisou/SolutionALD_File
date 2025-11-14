0f0636e216de485d213c07241297ea672b217052
Moussa <salisoumoussa1@gmail.com>
2025-11-14 11:02:18 +0100

    Update Ex

--- Esercizio/Program.cs
+++ Esercizio/Program.cs
@@ -1,11 +1,13 @@
 ﻿/*
  * Crea prima la truttura di base del program
- *    Aggiungi un messagio di (Venvenuto nel ristorate)
+ *    Aggiungi un messagio di (Venvenuto nel ristorate SushiRow)
  *    Chiedi all'utente se ha una riservazione (si/on)
  *    Se l'utente ristponde si (Con che nome?)
  *          l'utente scrive il suo mome 
  *          Messaggio (Ok (Nome dell'utente).......)          
  *     , no(Non habbiamo tavoli liberi per ora)
  *    Dopo la risposta dell'utente:
- *      Presenta il menu
-*/
+ *          Presenta il menu con (piatti e bevande = prezzi)
+ *    Chiedi all'utente se vuole ordinare qualcosa da bere?
+ *    Chiedi se voule il conto
+ *    Chiedi all'utente se peaga con Carta/Bancomat 
